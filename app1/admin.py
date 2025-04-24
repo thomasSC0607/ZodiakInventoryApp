eb4d755d7380bc78c242c9bf52b7ef0db104cbda
Maocampog1 <maocampog1@eafit.edu.co>
2025-04-24 09:14:38 -0500

    Update admin.py to register new models

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Empleado
 
-# Register your models here.
+class EmpleadoAdmin(UserAdmin):
+    model = Empleado
+    list_display = ['username', 'nombre', 'apellido', 'cedula']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Datos personales', {'fields': ('cedula', 'nombre', 'apellido')}),
+    )
+
+admin.site.register(Empleado, EmpleadoAdmin)
