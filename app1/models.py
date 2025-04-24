8592885409cfe5ec492d7c6f6e71fa20b0209687
Maocampog1 <maocampog1@eafit.edu.co>
2025-04-24 09:14:47 -0500

    Update models with new fields or classes

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,12 @@
-from django.db import models
 
-# Create your models here.
+# app1/models.py
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Empleado(AbstractUser):
+    cedula = models.CharField(max_length=15, unique=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
